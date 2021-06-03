fd91ce9c13aa8d1a4606c11b6e4fc622b1092263
sembiyan <sembiyanofficial@gmail.com>
2021-06-03 15:01:14 +0530

    completed left side in to-do

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-var a = -1;
-document.getElementsByClassName("fa-plus")[0].onclick = function() {
-    var i = document.createElement("Li");
-    i.setAttribute("class", "list-element");
-    var j = document.getElementById("add-element").value;
-    if ("" == j) {
-      a++;
-      i.innerHTML = "untitled("+a+")";
+var untitledListCount = -1;
+const listElements = ["My Day", "Important", "Planned", "Assigned to you", "Tasks"];
+const listIcons = ["far fa-sun", "far fa-star", "far fa-calendar-alt", "far fa-user", "fas fa-home"];
+var list = document.getElementById("left-details");
+for (var i = 0; i < listElements.length; i++) {
+    var listItem = document.createElement("Li");
+    listItem.setAttribute("class", "list-element");
+    var listIcon = document.createElement("i");
+    listIcon.setAttribute("class", listIcons[i]);
+    listItem.appendChild(listIcon);
+    var listText = document.createElement("span");
+    listItem.appendChild(listText);
+    listText.innerHTML = listElements[i];
+    list.appendChild(listItem);
+} 
+
+function newListElement(event) {
+  var key = event.keyCode;
+  if (13 == key) {
+    var listItem = document.createElement("Li");
+    listItem.setAttribute("class", "list-element");
+    var listIcon = document.createElement("i");
+    listIcon.setAttribute("class", "fas fa-list-ul");
+    listItem.appendChild(listIcon);
+    var listText = document.createElement("span");
+    listItem.appendChild(listText);
+    var newListItem = document.getElementById("add-element").value;
+    if ("" == newListItem) {
+      untitledListCount++;
+      listText.innerHTML = "Untitled list (" + untitledListCount + ")";
     } else {
-      i.innerHTML = j;
+      listText.innerHTML = newListItem;
     }
-    document.getElementById("left-details").appendChild(i);
+    list.appendChild(listItem);
+  }
 }
-var input = document.getElementById("add-element");
-input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-        
-        document.getElementsByClassName("fa-plus")[0].click();
-    }
-});
